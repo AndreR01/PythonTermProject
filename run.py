0f81cfd550c2122c4d6d2cbd99626d5a3a27bb33
Andre <blueringedoccy@gmail.com>
2022-03-31 13:42:30 -0230

    create layout page. add list of dishes

--- run.py
+++ run.py
@@ -3,7 +3,12 @@ app = Flask(__name__)
 
 @app.route("/")
 def rootRoute():
-    return render_template('mainpage.html', my_var="whats up")
+    dishNames = ["Easy-chicken-tikka-masala",
+                 "Smoked-pulled-pork-recipe",
+                 "Crock-pot-ribs-slow-cooker-ribs",
+                 "Best-twice-baked-potatoes-recipe",
+                 "6-ingredient-lazy-day-chili"]
+    return render_template('layout.html', my_var="HOME PAGE", dishNames = dishNames)
 
 if __name__ == '__main__':
     app.run(debug=True)

62f19b260a7dcf473dbf3c1e6b24b0059cda2d14
Andre <blueringedoccy@gmail.com>
2022-04-05 21:43:34 -0230

    add dishes.csv and modify functions in run.py

--- run.py
+++ run.py
@@ -1,3 +1,5 @@
+import csv
+
 from flask import Flask, render_template
 app = Flask(__name__)
 
@@ -9,6 +11,19 @@ def rootRoute():
                  "Best-twice-baked-potatoes-recipe",
                  "6-ingredient-lazy-day-chili"]
     return render_template('layout.html', my_var="HOME PAGE", dishNames = dishNames)
+
+@app.route("/dish/<dishName>")
+def showDish(dishName):
+    dish = {}
+    dishNames = []
+    with open('dishes.csv', newline='') as csvfile:
+        filereader = csv.reader(csvfile, delimiter=',', escapechar='\\')
+        for row in filereader:
+            dishNames.append(row[0])
+            if row[0] == dishName:
+                dish = row
+    return render_template('dish.html', dish_list=dishNames, dish=dish)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
